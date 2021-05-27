28bd971b0d6a9d0fbe8d2961441bba63c84e0807
NPrime01 <47795760+NPrime01@users.noreply.github.com>
2021-05-27 15:22:43 -0500

    Added JUnit tests

--- src/test/java/com/wordsearcher/WordsearcherApplicationTests.java
+++ src/test/java/com/wordsearcher/WordsearcherApplicationTests.java
@@ -1,13 +1,96 @@
 package com.wordsearcher;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.wordsearcher.Model.WordData;
+import com.wordsearcher.Repository.WordRepository;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.ResourceUtils;
+
+import jdk.nashorn.internal.runtime.logging.DebugLogger;
 
 @SpringBootTest
 class WordsearcherApplicationTests {
+	@Autowired
+	WordRepository wordRepository;
 
 	@Test
-	void contextLoads() {
+	void DatabaseLoadsCorrectly() {
+		List<Object[]> tokens = null;
+		try {
+			File file = ResourceUtils.getFile("classpath:static/words.txt");
+			tokens = Files.lines(file.toPath())
+				.map(word -> word.split(" "))
+				.collect(Collectors.toList()); 
+		}
+		catch(IOException e) {
+			// fail test if database not found
+			fail("Database not found");
+		}
+		
+		// make sure the entire database loaded into memory
+		assertEquals(tokens.size(), wordRepository.getAllPossible("").size());
 	}
 
+	@Test
+	void startsWithTest() {
+		// collect test data
+		List<String> actual = wordRepository.getAllPossible("Ry", "Starts With").stream()
+			.map(word -> word.getWord())
+			.collect(Collectors.toList());
+
+		// set up example data
+		List<String> expected = Arrays.asList("Ryan", "Ryann", "Ryazan", "Rybinsk", "Rycca",
+			"Rychard", "Rydal", "Rydberg", "Rydder", "Ryde", "Ryder", "Ryderwood", "Rye",
+			"Ryeland", "Ryland", "Ryle", "Ryley", "Rymandra", "Rynchospora", "Ryon", "Rysler", 
+			"Ryswick", "Ryter", "Rytina", "Ryukyu", "Ryun", "Ryunosuke", "Ryurik");
+
+		// make sure the querys match
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void endsWtihTest() {
+		// collect test data
+		List<String> actual = wordRepository.getAllPossible("axe", "Ends With").stream()
+			.map(word -> word.getWord())
+			.collect(Collectors.toList());
+		
+		// set up expected data
+		List<String> expected = Arrays.asList("Badaxe", "battle-axe", "breakaxe", "broadaxe",
+			"broad-axe", "curtaxe", "equiaxe", "pickaxe", "poleaxe", "pole-axe", "Saxe", "scramasaxe",
+			"stone-axe", "tommy-axe");
+		
+		// check actual vs expected
+		assertEquals(expected, actual);
+	}
+
+	@Test 
+	void containsTest() {
+		// collect test data
+		List<String> actual = wordRepository.getAllPossible("zoom", "Contains").stream()
+			.map(word -> word.getWord())
+			.collect(Collectors.toList());
+
+		// get actual data
+		List<String> expected = Arrays.asList("bazooms", "zoom", "zoomagnetic", "zoomagnetism",
+			"zoomancy", "zoomania", "zoomanias", "zoomantic", "zoomantist", "zoomechanical",
+			"zoomechanics", "zoomed", "zoomelanin", "zoometry", "zoometric", "zoometrical", 
+			"zoometries", "zoomimetic", "zoomimic", "zooming", "zoomorph", "zoomorphy", "zoomorphic",
+			"zoomorphism", "zoomorphize", "zoomorphs", "zooms");
+		
+		// check actual against expected
+		assertEquals(expected, actual);
+	}
 }
